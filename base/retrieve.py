0b02354091aac5e95b422f591df5b043d709821a
flaviusfetean <flavius_fetean@yahoo.com>
2024-04-12 21:59:20 +0300

    removed the question mark adaptation

--- base/retrieve.py
+++ base/retrieve.py
@@ -4,7 +4,7 @@ from base.openai import Assistant
 db = Database()
 assistant = Assistant()
 
-SIMILARITY_THRESHOLD = 0.25
+SIMILARITY_THRESHOLD = 0.15
 
 
 def get_closest_match(query: str):
@@ -21,16 +21,6 @@ def get_closest_match(query: str):
         return {"source": "local",
                 "matched_question": answer.page_content,
                 "answer": answer.metadata['answer']}
-    else:
-        if query[-1] != "?":  # if no match is found, try to add or remove question mark,
-            query += "?"      # as the question mark has a big impact on the embeddings and subsequently, similarity
-        else:
-            query = query[:-1]
-        answer, score = db.query_by_similarity(query, 1)[0]
-        if score < SIMILARITY_THRESHOLD:
-            return {"source": "local",
-                    "matched_question": answer.page_content,
-                    "answer": answer.metadata['answer']}
 
     print("No match found for: ", query)
     print("Forwarding to assistant")
