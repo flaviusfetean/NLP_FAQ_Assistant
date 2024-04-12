a3524a3d69357501dbc88a8f3fa818f74c99d6bb
flaviusfetean <flavius_fetean@yahoo.com>
2024-04-12 22:31:04 +0300

    added basic exception handling to api interaction

--- base/openai.py
+++ base/openai.py
@@ -1,4 +1,4 @@
-from openai import OpenAI
+import openai
 from config import api_key
 from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
@@ -33,20 +33,26 @@ class Assistant:
         print("Client type: ", self.client_type)
 
         if self.client_type == "openai":
-            response = self.client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": "You are a FAQ assistant."
-                                                  "Your answer should be clear and straightforward."
-                                                  "Do not include any unnecessary information."},
-                    {"role": "user", "content": question}
-                ],
-                temperature=0.1,
-                max_tokens=100
-            )
-            return {"source": "openai",
-                    "matched_question": "N/A",
-                    "answer": response.choices[0].message.content}
+            try:
+                response = self.client.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": "You are a FAQ assistant."
+                                                      "Your answer should be clear and straightforward."
+                                                      "Do not include any unnecessary information."},
+                        {"role": "user", "content": question}
+                    ],
+                    temperature=0.1,
+                    max_tokens=100
+                )
+                return {"source": "openai",
+                        "matched_question": "N/A",
+                        "answer": response.choices[0].message.content}
+            except openai.OpenAIError as e:  # handles RateLimit, Authentication, PermissionDenied, NotFound, etc
+                print(f"Error from Openai side: {e}. Using the local model")
+                self.switch_local()
+                return self.ask(question)
+
         else:
             print("Using local model")
             return {"source": "local-orca-mini",
@@ -54,7 +60,7 @@ class Assistant:
                     "answer": self.client.invoke({"input": question})}
 
     def init_client(self):
-        client = OpenAI(api_key=api_key)
+        client = openai.OpenAI(api_key=api_key)
 
         if not self.test_key(client):
             model = Ollama(model="orca-mini", base_url=f"http://{OLLAMA_HOST}:11434")
@@ -76,7 +82,7 @@ class Assistant:
 
     def register_key(self, key):
         if self.client_type == "local":
-            self.client = OpenAI(api_key=key)
+            self.client = openai.OpenAI(api_key=key)
             if self.test_key(self.client):
                 self.client_type = "openai"
                 print("Successfully connected to openai server")
@@ -84,3 +90,18 @@ class Assistant:
             else:
                 print("Invalid API key. Using local model instead.")
                 return False
+
+    def switch_local(self):
+        if self.client_type == "openai":
+            model = Ollama(model="orca-mini", base_url=f"http://{OLLAMA_HOST}:11434")
+            prompt = ChatPromptTemplate.from_messages([
+                ("system", "You are a FAQ assistant."
+                           "Your answer should be clear and straightforward."
+                           "Do not include any unnecessary information."),
+                ("user", "{input}")
+            ])
+            parser = StrOutputParser()
+            self.client = prompt | model | parser
+            self.client_type = "local"
+            print("Switched to local model")
+
